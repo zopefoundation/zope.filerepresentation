31cd8c10a38a47086eda00a0360792ab2a8efd56
Baiju Muthukadan <baiju.m.mail@gmail.com>
2006-10-27 06:00:43 +0000

    Initial commit of zope.filerepresentation for eggification.

---
+++ setup.py
@@ -0,0 +1,50 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.filerepresentation package
+
+$Id$
+"""
+
+import os
+
+try:
+    from setuptools import setup, Extension
+except ImportError, e:
+    from distutils.core import setup, Extension
+
+setup(name='zope.filerepresentation',
+      version='3.4-dev',
+      url='http://svn.zope.org/zope.filerepresentation',
+      license='ZPL 2.1',
+      description='Zope filerepresentation',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="File-system representation interfaces."
+                       "The interfaces defined here are used for"
+                       "file-system and file-system-like"
+                       "representations of objects, such as"
+                        "file-system synchronization, FTP,"
+                        "PUT, and WebDAV.",
+
+      packages=['zope', 'zope.filerepresentation'],
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['zope.interface',
+                        'zope.app.container',],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
