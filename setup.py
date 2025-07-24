9e3a375b072955cba345b71ecd5b28e69eb19564
Michael Howitz <icemac@gmx.net>
2025-07-24 08:37:15 +0200

    Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@
 """
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -62,9 +61,6 @@ setup(
     ],
     url='http://zopefilerepresentation.readthedocs.io',
     license='ZPL-2.1',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    namespace_packages=['zope'],
     install_requires=[
         'setuptools',
         'zope.interface',
@@ -72,7 +68,7 @@ setup(
     ],
     extras_require={
         'test': [
-            'zope.testrunner',
+            'zope.testrunner >= 6.4',
         ],
         'docs': [
             'Sphinx',
