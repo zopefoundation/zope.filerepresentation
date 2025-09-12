869e30be7ff28f7f84ba00d6e0eacfc9fe0a92eb
Michael Howitz <icemac@gmx.net>
2025-09-12 09:12:07 +0200

    Back to development: 7.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(
     name='zope.filerepresentation',
-    version='7.0',
+    version='7.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='File-system Representation Interfaces',
